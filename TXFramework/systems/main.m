3888a2327b4eef6e71c29b96e351d64981041ca7
TianXiao <331065255@163.com>
2015-01-21 11:58:21 +0800

    添加系统文件

---
+++ TXFramework/systems/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TXFramework
+//
+//  Created by tx on 15-1-21.
+//  Copyright (c) 2015年 doumee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
